00a0a1ec6d2ff761cc73b5df6f882b201663b52c
starodubov <sergei.starodubov@simbirsoft.com>
2020-04-06 13:43:07 +0400

    craete search ms

---
+++ search/src/main/java/com/search/hh/HhApplication.java
@@ -0,0 +1,15 @@
+package com.search.hh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class HhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhApplication.class, args);
+    }
+
+}
