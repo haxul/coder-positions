432b4b2f20562b8668c43ad654cd1e0e492a1eaf
starodubov <sergei.starodubov@simbirsoft.com>
2020-04-06 13:24:48 +0400

    init

---
+++ codersPositions/src/test/java/com/coders/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coders.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
